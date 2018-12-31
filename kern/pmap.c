5e119c86244593849cc5665e136d54b2956180fe
eldar <eldar.haseljic@fet.ba>
2018-12-31 21:56:44 +0100

    mmio_map_region() done

--- kern/pmap.c
+++ kern/pmap.c
@@ -666,7 +666,21 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+  
+  void * virtualaddr = (void *) base;
+  size_t length = ROUNDUP(size, PGSIZE);
+  
+  if( base + length > MMIOLIM )
+    panic("mmio_map_region() : out of memory \n");
+
+  boot_map_region( kern_pgdir, (uintptr_t) virtualaddr, length, pa, 
+                  PTE_P | PTE_W | PTE_PCD | PTE_PWT );
+
+  base += length;
+  
+  invlpg(virtualaddr);
+  return virtualaddr;
+  //  panic("mmio_map_region not implemented");
 }
 
 static uintptr_t user_mem_check_addr;
